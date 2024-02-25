0e513636a765fa344c5a45defff300420f731df5
Artem Makovskyy <photomak79@gmail.com>
2024-02-25 22:51:11 +0200

    update db

--- src/main/java/com/example/d2tech/D2techApplication.java
+++ src/main/java/com/example/d2tech/D2techApplication.java
@@ -1,13 +1,18 @@
 package com.example.d2tech;
 
+import javax.swing.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class D2techApplication {
+//    Railway.App - Part
+//    https://www.youtube.com/watch?v=axSw17qI-5c&list=PLA7e3zmT6XQX6uZ2kC5R7exF0glolxs_c&index=3
+    //next one free db
+//    https://www.youtube.com/watch?v=TMGHOW8Hzvw&t=27s
+//    https://www.freesqldatabase.com/
 
     public static void main(String[] args) {
         SpringApplication.run(D2techApplication.class, args);
     }
-
 }
